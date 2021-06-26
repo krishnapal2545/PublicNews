ed693679832b6eac59c61b843f953f04a3026a00
Krishna Pal <Krishnapal2545@gmail.com>
2021-06-26 18:32:08 +0530

    async function and chats

--- app.py
+++ app.py
@@ -3,39 +3,39 @@ from werkzeug.utils import secure_filename
 import os
 from flask import Flask , render_template , request , redirect , url_for , flash , session
 from markupsafe import escape
-import string,random , datetime , math
+import string,random , datetime , math , time
 from flask_sqlalchemy import SQLAlchemy
+
 
 with open('config.json','r') as c:
     params = json.load(c)["params"]
 
-
 UPLOAD_FOLDER = 'static/'
 ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg', }
 app = Flask(__name__)
-pool_size=20  
+pool_size=20
 max_overflow=0
 # if params['local_server']:
 #     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
 # else:
 #     app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
 app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
-
 app.config['SECRET_KEY'] = "sending messages"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16* 1024 *1024
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class User_credential(db.Model):
 
+class User_credential(db.Model):
+    __tablename__ = 'user_credential'
     ID_Number = db.Column(db.String(10) ,primary_key = True)
-    Phone_Number = db.Column(db.String(15) )
-    Username = db.Column(db.String(50) )
-    Password = db.Column(db.String(50) )
+    Phone_Number = db.Column(db.String(15))
+    Username = db.Column(db.String(50))
+    Password = db.Column(db.String(50))
 
 class Profile_info(db.Model):
-    
+    __tablename__ = 'profile_info'
     id = db.Column(db.Integer, primary_key=True)
     ID_Number = db.Column(db.String(10),db.ForeignKey('user_credential.ID_Number'),primary_key=True)
     Name = db.Column(db.String(150))
@@ -48,7 +48,7 @@ class Profile_info(db.Model):
 
 
 class News(db.Model):
-    
+    __tablename__ = 'news'
     Srno = db.Column(db.Integer, primary_key=True)
     User_ID = db.Column(db.String(10),primary_key=True)
     News_ID = db.Column(db.String(150),primary_key=True)
@@ -61,19 +61,19 @@ class News(db.Model):
     Reading = db.relationship('Reading_list',lazy ='dynamic')
     
 class Reading_list(db.Model):
-    
+    __tablename__ = 'reading_list'
     id = db.Column(db.Integer, primary_key=True)
     News_ID = db.Column(db.String(150),db.ForeignKey('news.News_ID'))
     User_ID = db.Column(db.String(100))
 
 class Followed(db.Model):
-    
+    __tablename__ = 'followed'
     id = db.Column(db.Integer, primary_key=True)
     User_ID = db.Column(db.String(100),db.ForeignKey('profile_info.ID_Number'))
     Followed_ID = db.Column(db.String(100))
 
 class Contact(db.Model):
-
+    __tablename__ = 'contact'
     id = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(100))
     Email = db.Column(db.String(100))
@@ -81,8 +81,9 @@ class Contact(db.Model):
 
 
 @app.route('/')
-def home():
-    No_post = params['no_of_post']
+async def home():
+    tic = time.perf_counter()
+    No_post =  params['no_of_post']
     Article = News.query.all()
     Article.reverse()
     last = math.ceil( len(Article)/int(No_post))
@@ -100,14 +101,17 @@ def home():
     else:
         prev = "/?page="+ str(page-1)
         old = "/?page="+ str(page+1)
+    toc = time.perf_counter()
+    print(toc - tic)
     if 'id_no' in session :
         return render_template('home.html',ID_Number = escape(session['id_no']),Article=Article,prev=prev,old=old,num=page,last=last)
-    else:
-        return render_template('home.html',Article=Article,prev=prev,old=old,num=page,last=last)
+    
+    return render_template('home.html',Article=Article,prev=prev,old=old,num=page,last=last)
 
 
 @app.route('/repoter',methods=['GET', 'POST'])
-def login_register():
+async def login_register():
+    tic = time.perf_counter()
     if request.method == 'POST':
         if request.form['tab']== '1':  
             id_no = request.form['id_no']
@@ -141,12 +145,14 @@ def login_register():
             db.session.add(create)
             db.session.commit()
             flash(f'Your Registration is done and Your ID Number is {id_no}')
-
+    toc = time.perf_counter()
+    print(toc - tic)
     return render_template('login_register.html')
 
 @app.route('/profile/<user_id>', methods = ['GET','POST'])
 @app.route('/profile', methods = ['GET','POST'])
-def profile(user_id=None):
+async def profile(user_id=None):
+    tic = time.perf_counter()
     if 'id_no' in session :
         id_no =str(escape(session['id_no']))
         otheruser = False
@@ -202,14 +208,15 @@ def profile(user_id=None):
                   db.session.commit()
               else:
                   pass
-
+        toc = time.perf_counter()
+        print(toc - tic)
         return render_template('profile.html',User = User,Article = article,otheruser=otheruser,Follower = follower,Followed=followed,Reading = reading,Alluser = alluser,Allnews = allnews)
     else:
         return redirect(url_for('login_register'))
 
 
 @app.route('/logout')
-def logout():
+async def logout():
     if 'id_no' in session :
         session.pop('id_no', None)
         return redirect(url_for('home'))
@@ -218,7 +225,8 @@ def logout():
 
 
 @app.route('/addnews',methods=['GET', 'POST'])
-def addnews():
+async def addnews():
+    tic = time.perf_counter()
     if 'id_no' in session:
         if request.method == 'POST':
             while True :
@@ -238,8 +246,11 @@ def addnews():
             create = News(User_ID = id_no,News_ID = news_id,Thumbnail = thumb,Location = locat,Tag=tag,Description = descrip,Title = title,Date =date1)
             db.session.add(create)
             db.session.commit()
+            toc = time.perf_counter()
+            print(toc - tic)
             return redirect(url_for('profile'))
-
+        toc = time.perf_counter()
+        print(toc - tic)
         return render_template('addnews.html')
     else:
         return redirect(url_for('home'))
@@ -247,7 +258,7 @@ def addnews():
 
 @app.route('/news')
 @app.route('/news/<article>',methods=['GET','POST'])
-def news(article=None):
+async def news(article=None):
     owner = False
     unknow = True
     if article:
@@ -290,6 +301,10 @@ def news(article=None):
                  prev = "/news/"+ article +"/?page="+ str(page-1)
                  old = "/news/"+ article +"/?page="+ str(page+1)
               return render_template('newspage.html',Article= Article,Owner=owner,prev=prev,old=old,num=page,last=last)
+           else:
+                flash('NEWS is NOT Available Sorry !!')
+                return redirect(url_for('home'))
+
         except Exception as e:
             flash('NEWS is NOT Available Sorry !!')
             return redirect(url_for('home'))
@@ -300,7 +315,7 @@ def news(article=None):
 
 @app.route('/editnews')
 @app.route('/editnews/<article>', methods = ['GET','POST'])
-def editnews(article=None):
+async def editnews(article=None):
     if 'id_no' in session:
       if article:
         if request.method == 'POST':
@@ -335,8 +350,13 @@ def contact():
 
     return render_template('contact.html')
 
+@app.before_request
+async def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = datetime.timedelta(minutes=60)
+
 @app.route('/news/delete/<article>')
-def deletenews(article):
+async def deletenews(article):
     if 'id_no' in session :
         id_no = str(escape(session['id_no']))
         Article = News.query.filter_by(News_ID = article,User_ID = id_no).first()
@@ -348,6 +368,13 @@ def deletenews(article):
             return redirect(url_for('login_register'))
     else:
         return redirect(url_for('login_register'))
+
+@app.route('/chats/<friend>')
+async def chats(friend):
+    if 'id_no' in session:
+        return 'Working in Progress'
+    else:
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
    db.create_all()
